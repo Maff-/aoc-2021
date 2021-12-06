3daf946db6ce89328234f541c2a10a9fe3e01b4b
Ruud Bijnen <r.bijnen@gmail.com>
2021-12-06 06:45:42 +0100

    Day 6 part 2

--- day06/run.php
+++ day06/run.php
@@ -8,13 +8,13 @@ EXMAPLE;
 
 $input = file_get_contents('input.txt');
 
-$input = explode(",", trim($input));
+$input = explode(',', trim($input));
 $input = array_map('intval', $input);
 
 // Part 1
 
-$foo = 7;
-$bar = 2;
+$period = 7;
+$init = 2;
 $days = 80;
 
 $state = $input;
@@ -23,12 +23,29 @@ for ($d = 0; $d < $days; $d++) {
     for ($n = 0; $n < $count; $n++) {
         $timer = $state[$n];
         if ($timer === 0) {
-            $state[$n] = $foo - 1;
-            $state[] = $bar + $foo - 1;
+            $state[$n] = $period - 1;
+            $state[] = $init + $period - 1;
         } else {
             $state[$n]--;
         }
     }
 }
 
-echo '[Part 1] Number of lanternfish after 80 days: ', count($state), \PHP_EOL;
+echo '[Part 1] Number of lanternfish after ', $days, ' days: ', count($state), \PHP_EOL;
+
+// Part 2 - a more efficient way
+
+$days = 256;
+
+$state = array_fill(0, $init + $period, 0);
+$state = array_replace($state, array_count_values($input));
+for ($d = 0; $d < $days; $d++) {
+    $done = $state[0] ?? 0;
+    for ($t = 1; $t < ($init + $period); $t++) {
+        $state[$t - 1] = $state[$t];
+    }
+    $state[$period - 1] += $done;
+    $state[$init + $period - 1] = $done;
+}
+
+echo '[Part 2] Number of lanternfish after ', $days, ' days: ', array_sum($state), \PHP_EOL;
