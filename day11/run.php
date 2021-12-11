c21d6f651df9e759efeb5a917a9bce98039a2d71
Ruud Bijnen <r.bijnen@gmail.com>
2021-12-11 10:55:10 +0100

    Day 11 part 2

--- day11/run.php
+++ day11/run.php
@@ -77,3 +77,61 @@ for ($step = 1; $step <= $steps; $step++) {
 }
 
 echo '[Part 1] total flashes after ', $steps ,' steps: ', $totalFlashes, \PHP_EOL;
+
+// Part 2
+
+$data = $input;
+$step = 1;
+$octopuses = $width * $height;
+
+while (true) {
+    // First, the energy level of each octopus increases by 1.
+    for ($y = 0; $y < $height; $y++) {
+        for ($x = 0; $x < $width; $x++) {
+            $data[$y][$x]++;
+        }
+    }
+
+    // cascade flashing
+    $flashes = 0;
+    $flashMap = array_fill(0, $height, array_fill(0, $width, false));
+    while (true) {
+        $flashed = false;
+        for ($y = 0; $y < $height; $y++) {
+            for ($x = 0; $x < $width; $x++) {
+                if ($data[$y][$x] > 9 && !$flashMap[$y][$x]) {
+                    $flashed = true;
+                    $flashMap[$y][$x] = true;
+                    foreach (DIRECTIONS as [$dY, $dX]) {
+                        [$aY, $aX] = [$y + $dY, $x + $dX];
+                        if ($aY < 0 || $aY >= $height || $aX < 0 || $aX >= $width) {
+                            continue;
+                        }
+                        $data[$aY][$aX]++;
+                    }
+                }
+            }
+        }
+        if (!$flashed) {
+            break;
+        }
+    }
+
+    // any octopus that flashed during this step has its energy level set to 0
+    for ($y = 0; $y < $height; $y++) {
+        for ($x = 0; $x < $width; $x++) {
+            if ($data[$y][$x] > 9) {
+                $flashes++;
+                $data[$y][$x] = 0;
+            }
+        }
+    }
+
+    if ($flashes === $octopuses) {
+        break;
+    }
+
+    $step++;
+}
+
+echo '[Part 2] first step during which all octopuses flash: ', $step, \PHP_EOL;
